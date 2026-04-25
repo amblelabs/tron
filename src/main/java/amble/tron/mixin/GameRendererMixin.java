5ab1d3424d1b1935dba5b2cc92d824b438c3c1a1
Loqor <isaiahcrace@gmail.com>
2026-04-24 22:30:34 -0500

    camera modifciation

---
+++ src/main/java/amble/tron/mixin/GameRendererMixin.java
@@ -0,0 +1,22 @@
+package amble.tron.mixin;
+import amble.tron.core.entities.LightCycleEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RotationAxis;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+    @Inject(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/GameRenderer;bobView(Lnet/minecraft/client/util/math/MatrixStack;F)V"))
+    private void tron$renderWorld(float tickDelta, long limitTime, MatrixStack matrices, CallbackInfo ci) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player != null && client.player.getVehicle() instanceof LightCycleEntity cycle) {
+            float currentTilt = MathHelper.lerp(tickDelta, cycle.prevTilt, cycle.tilt);
+            matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(-currentTilt));
+        }
+    }
+}
